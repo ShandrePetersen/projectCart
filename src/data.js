a453351f0cd8c555b86d9a97b2756adda85a5fa6
Shandre Petersen <150180183+ShandrePetersen@users.noreply.github.com>
2024-02-29 16:21:25 +0200

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,25 @@
+let shopItemsData = [{
+    id:"greenRolex",
+    name: "Green Rolex",
+    price:  150,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Rolex1.jpg"
+}, 
+{
+    id:"silverRolex",
+    name: "Silver Rolex",
+    price:  300,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Rolex2.jpg"}, 
+{
+    id:"blackTag",
+    name: "Black Tag",
+    price:  200,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Tag H2.jpg"}, 
+{
+    id:"silverTag",
+    name: "Silver Tag",
+    price:  250,
+    descr: "Lorem ipsum dolor, sit amet consectetur adipisicing.",
+    img: "images/Tag H3.jpg"}];
